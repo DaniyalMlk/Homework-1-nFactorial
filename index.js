aaa2c7956edbb01c7007f7f1fada42e96c23b7ff
Daniyal Malik <danmalik@MacBook-Pro-Daniyal.local>
2024-05-03 22:57:50 +0500

    homework6

--- index.js
+++ index.js
@@ -10,32 +10,95 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const ageTable = document.getElementById('age-table');
-console.log(ageTable); 
-
-const label = ageTable.getElementsByTagName('label');
-console.log(label);
-
-const td = ageTable.getElementsByTagName('td')[0];
-console.log(td);
-
-const search = document.querySelector('form[name="search"]');
-console.log(search);
-
-const firstInput = search.querySelector('input');
-console.log(firstInput);
-
-const lastInput = search.querySelector('input:last-child');
-console.log(lastInput);
+// / DOM element onclick
+// const btn = document.querySelector('.btn'),
+//       btn1 = document.querySelectorAll('.btn')[1],
+//       btn2 = document.querySelectorAll('.btn')[2],
+//       body = document.querySelector('body'),
+//       btns = document.querySelectorAll('.btn');
 
 
-//Task 2
-{/* <script>
-  let body = document.body;
+// // btn.onclick = function(){
+// //     alert('Hello!')
+// // }
 
-  body.innerHTML = "<!--" + body.tagName + "-->";
+// // btn.onclick = function(){
+// //     alert('Hello again!');
+// // }
 
-  alert( body.firstChild.data ); // что выведет?
-</script> */}
 
-//ВЫВЕЛ aler() с надписью BODY;
+// const alertShow = function(event){
+//    console.log('My first click ')
+//    event.target.remove();
+//    btn.removeEventListener('click', alertShow)
+// }
+
+// btn.addEventListener('click', alertShow)
+
+// // btn.addEventListener('click', (event)=>{
+// //     event.target.remove();
+// // });
+
+// let i = 0
+
+// const count = function(){
+//     console.log(i, 'i increacing')
+//     i++;
+//     if(i===3){
+//         btn1.removeEventListener('click', count);
+//     }
+// }
+
+// btn1.addEventListener('click', count);
+
+// btn2.addEventListener('mouseover', function(e){
+//     console.log(e)
+//     btn2.style.backgroundColor = 'black';
+// })
+
+// btn2.addEventListener('mouseout', function(e){
+//     console.log(e)
+//     btn2.style.backgroundColor = 'blue';
+// })
+
+// let s = ''
+// body.addEventListener('keydown',(e)=>{
+//     s+= e.key;
+//     if(s === 'dalida'){
+//         alert('you texted to Dalida');
+//     }
+// })
+
+// btns.forEach(item=>{
+//     item.addEventListener('click', ()=>{
+//         console.log('count')
+//     })
+// });
+
+
+////////////////////////////
+
+//1st Task
+// document.getElementById('hider').onclick = function() {
+//     let text = document.getElementById('text');
+//     text.style.visibility = 'hidden';  
+// };
+
+//2nd Task
+
+// const button = document.querySelector('button');
+
+// button.addEventListener("click", () => alert("1"));
+// button.removeEventListener("click", () => alert("1"));
+// button.onclick = () => alert(2);
+//Ответ 1 и 2
+
+//3rd Task
+
+// let panes = document.querySelectorAll('.pane');
+
+//     for(let pane of panes) {
+//       pane.insertAdjacentHTML("afterbegin", '<button class="remove-button">[x]</button>');
+//       pane.firstChild.onclick = () => pane.remove();
+//     }
+//Cори, код юзанул с песочницы, других вариантов в голову не пришло, решение понял прочитал как юзается insert
